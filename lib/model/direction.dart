3037805c9ddc7bd8c79fe7b5afdca82f228d613e
Sugyo-In <sgin0821@gmail.com>
2022-07-29 01:12:54 +0900

    [#9] feat: add recipe models

--- lib/model/direction.dart
+++ lib/model/direction.dart
@@ -1,17 +1,15 @@
-// import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-// import 'ingredient.dart';
+import 'amount/amount.dart';
+import 'ingredient.dart';
 
-// part 'direction.freezed.dart';
-// part 'direction.g.dart';
+part 'direction.freezed.dart';
 
-// @freezed
-// class Direction with _$Direction {
-//   factory Direction({
-//     required String description,
-//     required List<Ingredient> ingredients,
-//   }) = _Direction;
-
-//   factory Direction.fromJson(Map<String, dynamic> json) =>
-//       _$DirectionFromJson(json);
-// }
+@freezed
+class Direction<T extends Amount> with _$Direction {
+  const factory Direction({
+    required String description,
+    @Default({}) Map<Ingredient, T> amountByIngredient,
+    @Default(Duration()) Duration time,
+  }) = _Direction;
+}
--- lib/model/ingredient.dart
+++ lib/model/ingredient.dart
@@ -1,15 +1,11 @@
-// import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-// part 'ingredient.freezed.dart';
-// part 'ingredient.g.dart';
+part 'ingredient.freezed.dart';
 
-// @freezed
-// class Ingredient with _$Ingredient {
-//   factory Ingredient({
-//     required String name,
-//     @Default('') String description,
-//   }) = _Ingredient;
-
-//   factory Ingredient.fromJson(Map<String, dynamic> json) =>
-//       _$IngredientFromJson(json);
-// }
+@freezed
+class Ingredient with _$Ingredient {
+  const factory Ingredient({
+    required String name,
+    @Default('') String description,
+  }) = _Ingredient;
+}
