e74aea98a90c48bdf8c276bd169b7107ef509857
Sugyo-In <sgin0821@gmail.com>
2022-12-13 00:50:05 +0900

    [#23] test: add test case for recipe local datasource

---
+++ data/test/datasource/local/recipe_local_datasource_test.dart
@@ -0,0 +1,55 @@
+import 'package:test/test.dart';
+
+void main() async {
+  group('[Recipe]', () {
+    test(
+      'should return added recipe object with id after adding the recipe',
+      () {},
+    );
+
+    test('Isar should be closed after adding the recipe', () {});
+
+    test('should return recipe object match with the given id', () {});
+
+    test(
+      'should throw the exception when failed to find match data with the given id',
+      () {},
+    );
+
+    test('Isar should be closed after searching the recipe', () {});
+
+    test('the recipe should not be found after deletion', () {});
+
+    test('Isar should be closed after updating the recipe', () {});
+
+    test('the recipe should be changed after the update', () {});
+
+    test('Isar should be closed after deleting the recipe', () {});
+  });
+
+  group('[Ingredient]', () {
+    test(
+      'should return added ingredient object with id after adding the ingredient',
+      () {},
+    );
+
+    test('Isar should be closed after adding the ingredient', () {});
+
+    test('should return ingredient object match with the given id', () {});
+
+    test(
+      'should throw the exception when failed to find match data with the given id',
+      () {},
+    );
+
+    test('Isar should be closed after searching the ingredient', () {});
+
+    test('the ingredient should not be found after deletion', () {});
+
+    test('Isar should be closed after updating the ingredient', () {});
+
+    test('the ingredient should be changed after the update', () {});
+
+    test('Isar should be closed after deleting the ingredient', () {});
+  });
+}
