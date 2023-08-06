df4aac29908df61b1e85160164e8795fa6f16067
Sugyo-In <sgin0821@gmail.com>
2023-08-06 16:49:45 +0900

    feat: 레시피 등록 양식 구현

--- lib/feature/edit_recipe_form/edit_recipe_form_state.dart
+++ lib/feature/edit_recipe_form/edit_recipe_form_state.dart
@@ -2,10 +2,21 @@ part of 'edit_recipe_form_feature.dart';
 
 @freezed
 class EditRecipeFormState with _$EditRecipeFormState {
+  const EditRecipeFormState._();
+
   const factory EditRecipeFormState({
     @Default('') String name,
     @Default('') String description,
     @Default([]) List<Direction> directions,
     @Default(1) int servings,
   }) = _EditRecipeFormState;
+
+  bool get isValid => name.isNotEmpty && directions.isNotEmpty && servings > 0;
+
+  Recipe get recipe => Recipe(
+        description: description,
+        directions: directions,
+        name: name,
+        servings: servings,
+      );
 }
--- lib/view/page/recipe/edit/form/recipe_form.dart
+++ lib/view/page/recipe/edit/form/recipe_form.dart
@@ -1,109 +1,117 @@
+import 'package:cooking_calulator/feature/feature.dart';
+import 'package:cooking_calulator/view/view.dart';
+import 'package:data/data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../recipe_provider.dart';
-import '../edit.dart';
+class RecipeForm extends ConsumerStatefulWidget {
+  const RecipeForm({
+    super.key,
+    this.recipe,
+    this.onSubmitted,
+  });
 
-class RecipeForm extends ConsumerWidget {
-  const RecipeForm({super.key});
+  final Recipe? recipe;
+
+  final ValueChanged<Recipe>? onSubmitted;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final recipeNotifier = ref.watch(recipeProvider.notifier);
-    final recipe = ref.watch(recipeProvider);
-    final name = recipe.name;
-    final description = recipe.description;
-    final servings = recipe.servings;
-    final nextButtonEnabled = recipe.name.isNotEmpty;
+  ConsumerState<RecipeForm> createState() => _RecipeFormState();
+}
+
+class _RecipeFormState extends ConsumerState<RecipeForm> {
+  late final AutoDisposeFamilyNotifierProvider<
+      EditRecipeFormIntent,
+      EditRecipeFormState,
+      Recipe?> _intentProvider = editRecipeFormIntentProvider(
+    widget.recipe,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final intent = ref.read(_intentProvider.notifier);
+    final state = ref.watch(_intentProvider);
 
     return Column(
       children: [
-        Expanded(
-          child: Form(
-            child: ListView(
-              children: [
-                TextFormField(
-                  initialValue: name,
-                  decoration: const InputDecoration(labelText: '이름'),
-                  autofocus: true,
-                  onChanged: (text) => recipeNotifier.update(
-                    (state) => state.copyWith(name: text),
-                  ),
-                  textInputAction: TextInputAction.next,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    IconButton(
-                      onPressed: () {
-                        if (servings <= 1) return;
-
-                        recipeNotifier.update(
-                          (state) => state.copyWith(servings: servings - 1),
-                        );
-                      },
-                      icon: const Icon(Icons.remove),
-                    ),
-                    Text('$servings 인분'),
-                    IconButton(
-                      onPressed: () => recipeNotifier.update(
-                        (state) => state.copyWith(servings: servings + 1),
-                      ),
-                      icon: const Icon(Icons.add),
-                    ),
-                  ],
-                ),
-                TextFormField(
-                  initialValue: description,
-                  decoration: const InputDecoration(labelText: '설명'),
-                  maxLines: 3,
-                  onChanged: (text) => recipeNotifier.update(
-                    (state) => state.copyWith(description: text),
-                  ),
-                  textInputAction: TextInputAction.newline,
-                ),
-                ...recipe.directions.map((e) => Text('test')),
-                OutlinedButton(
-                  onPressed: () async {
-                    final direction = await Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const EditDirectionPage(),
-                        fullscreenDialog: true,
-                        settings: const RouteSettings(
-                          name: EditDirectionPage.routeName,
-                        ),
-                      ),
-                    );
-
-                    recipeNotifier.update((state) => state.copyWith(
-                          directions: [...recipe.directions, direction],
-                        ));
-                  },
-                  child: Text('단계 추가'),
-                ),
-              ],
-            ),
-          ),
+        TextFormField(
+          initialValue: state.name,
+          decoration: const InputDecoration(labelText: '이름'),
+          autofocus: true,
+          onChanged: intent.setName,
+          textInputAction: TextInputAction.next,
         ),
-        ButtonBar(
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-              onPressed: null,
-              child: const Text('미리보기'),
+            IconButton(
+              onPressed: () => intent.setServings(state.servings - 1),
+              icon: const Icon(Icons.remove),
             ),
-            ElevatedButton(
-              onPressed: nextButtonEnabled
-                  ? () => _onNextButtonPressed(context)
-                  : null,
-              child: const Text('다음'),
+            Text('${state.servings} 인분'),
+            IconButton(
+              onPressed: () => intent.setServings(state.servings + 1),
+              icon: const Icon(Icons.add),
             ),
           ],
         ),
+        TextFormField(
+          initialValue: state.description,
+          decoration: const InputDecoration(labelText: '설명'),
+          maxLines: 3,
+          onChanged: intent.setDescription,
+          textInputAction: TextInputAction.newline,
+        ),
+        ReorderableListView(
+          onReorder: intent.reorderDirection,
+          shrinkWrap: true,
+          children: state.directions
+              .map((direction) => Row(
+                    key: ValueKey(direction),
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        direction.description,
+                      ),
+                      IconButton(
+                        onPressed: () => intent.removeDirection(direction),
+                        icon: const Icon(Icons.delete),
+                      )
+                    ],
+                  ))
+              .toList(),
+        ),
+        OutlinedButton(
+          onPressed: _onAddDirectionButtonPressed,
+          child: const Text('절차 추가'),
+        ),
+        const Spacer(),
+        if (state.isValid)
+          ElevatedButton(
+            onPressed: _onSubmitButtonPressed,
+            child: const Text('저장하기'),
+          ),
       ],
     );
   }
 
-  void _onNextButtonPressed(BuildContext context) {
-    Navigator.of(context).pushNamed(EditDirectionPage.routeName);
+  void _onAddDirectionButtonPressed() async {
+    final direction = await Navigator.of(context).push<Direction>(
+      MaterialPageRoute(
+        builder: (context) => const EditDirectionPage(),
+        fullscreenDialog: true,
+        settings: const RouteSettings(
+          name: EditDirectionPage.routeName,
+        ),
+      ),
+    );
+
+    if (direction == null) return;
+
+    ref.read(_intentProvider.notifier).addDirection(direction);
+  }
+
+  void _onSubmitButtonPressed() {
+    widget.onSubmitted?.call(ref.read(_intentProvider).recipe);
   }
 }
